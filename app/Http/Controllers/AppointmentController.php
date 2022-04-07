af97bb32ae3b8336d97c959c55ea612ff6a5710f
afdalabdallah <90978855+afdalabdallah@users.noreply.github.com>
2022-04-07 16:34:52 +0700

    Update AppointmentController.php

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Auth;
 
 class AppointmentController extends Controller
 {
-    public function __construct(){
+    public function __construct(){\
+        $this->middleware('auth');
         $this->Appointment = new Appointment();
     }
     
